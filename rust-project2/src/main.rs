f84d3dd21a5dc5504198d9894834ee8f4b79c5b5
SHORYA BAJ <jain.shorya11@gmail.com>
2024-08-11 01:23:54 +0530

    some structz more

--- rust-project2/src/main.rs
+++ rust-project2/src/main.rs
@@ -13,6 +13,9 @@ impl Rect {
     fn area(&self) -> u32 {
          self.width * self.height
     }
+    fn perimeter(&self) -> u32 {
+         2*(self.width + self.height)
+    }
 }
 
 fn main() {
@@ -22,10 +25,11 @@ fn main() {
         happy: true,
         
     };
-    print!("User {} whose age is {} is happy {}", user1.name ,user1.age ,user1.happy);
+    println!("User {} whose age is {} is happy {}", user1.name ,user1.age ,user1.happy);
     let rect = Rect {
         width: 30,
         height: 50,
     };
-    print!("The area of the rectangle is {}", rect.area());
+    println!("The area of the rectangle is {}", rect.area());
+    println!("The perimeter of the rectangle is {}", rect.perimeter());
 }
