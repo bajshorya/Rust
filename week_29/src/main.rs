1547bd77da16b32b672e557f918756acf5f01b0d
SHORYA BAJ <jain.shorya11@gmail.com>
2025-04-14 22:33:49 +0530

    generics in structs and functions

--- week_29/src/main.rs
+++ week_29/src/main.rs
@@ -1,8 +1,47 @@
-use chrono::prelude::*;
-fn main() {
-    let utc: DateTime<Utc> = Utc::now(); 
-    let local = Local::now(); 
+use std::ops::Mul;
 
+struct Rect<T>{ // struct to generics 
+    width : T,
+    height : T
+} 
 
-    println!("Hello, world!, the current time is :{} and {}",utc,local);
+impl<T> Rect<T> where T: Mul<Output =T> +Copy, {
+    fn area(&self) -> T{
+        return self.width * self.height;
+    }
 }
+
+fn main(){
+    // display_element(1,2);
+    // display_element(String::from("harkirat"),String::from("Shorya"));
+    let r = Rect{
+        width:10,
+        height:20
+    };
+    print!("area is:{}",r.area());
+}
+
+
+
+
+// fn display_element<T: std::fmt::Display>(a:T,b:T){
+//     println!("{}",a);
+//     println!("{}",b);
+// }
+// impl Rect<i32>{
+//     fn area(&self) -> i32{
+//         return self.width* self.height;
+//     }
+// }
+// use chrono::prelude::*;
+// fn main() {
+//     let utc: DateTime<Utc> = Utc::now(); 
+//     let local = Local::now(); 
+
+
+//     println!("Hello, world!, the current time is :{} and {}",utc,local);
+// }
+
+// struct User{
+//     username:String
+// }
